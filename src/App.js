640ae87269a1f3dccb79638343038b48bb5c7741
Milos4 <miso.27@hotmail.com>
2025-05-29 21:42:47 +0200

    ScrollToTop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './styles/colors.css';
-
-
+import "./styles/colors.css";
 
 //Pages
 import HomePage from "./pages/HomePage";
@@ -23,40 +21,42 @@ import FavoritesPage from "./pages/FavoritesPage";
 import FAQPage from "./pages/FAQPage";
 import ProductDetailsPage from "./pages/ProductDeatilsPage";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 ////
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/pocetna" element={<HomePage />} />
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pocetna" element={<HomePage />} />
 
-      <Route path="/o-nama" element={<AboutPage />} />
-      <Route path="/kontakt" element={<ContactPage />} />
-      <Route path="/reklamacije" element={<ComplaintsPage />} />
-      <Route path="/koristnicka-podrska" element={<CustomerSupportPage />} />
-      <Route path="/info-dostava" element={<DeliveryInformationPage />} />
-      <Route path="/uslovi-koristenja" element={<TermsOfUsePage />} />
-      <Route path="/pitanja" element={<FAQPage />} />
+        <Route path="/o-nama" element={<AboutPage />} />
+        <Route path="/kontakt" element={<ContactPage />} />
+        <Route path="/reklamacije" element={<ComplaintsPage />} />
+        <Route path="/koristnicka-podrska" element={<CustomerSupportPage />} />
+        <Route path="/info-dostava" element={<DeliveryInformationPage />} />
+        <Route path="/uslovi-koristenja" element={<TermsOfUsePage />} />
+        <Route path="/pitanja" element={<FAQPage />} />
 
-      <Route path="/korpa" element={<CartProductsPage />} />
-      <Route path="/prodavnica1" element={<ShopPage />} />
-      <Route path="/informacije-dostave" element={<OrderingProcessPage />} />
+        <Route path="/korpa" element={<CartProductsPage />} />
+        <Route path="/prodavnica1" element={<ShopPage />} />
+        <Route path="/informacije-dostave" element={<OrderingProcessPage />} />
 
-
-      <Route path="/prijava" element={<LoginPage />} />
-      <Route path="/registracija" element={<RegistrationPage />} />
-      <Route path="/istorija" element={<OrderHistoryPage />} />
-      <Route path="/favoriti" element={<FavoritesPage />} />
-      <Route path="/zaboravljenja-sifra" element={<ForgottenPasswordPage />} />
-      <Route path="/prodavnica" element={<ProductDetailsPage />} />
-
-
-
-
-    </Routes>
+        <Route path="/prijava" element={<LoginPage />} />
+        <Route path="/registracija" element={<RegistrationPage />} />
+        <Route path="/istorija" element={<OrderHistoryPage />} />
+        <Route path="/favoriti" element={<FavoritesPage />} />
+        <Route
+          path="/zaboravljenja-sifra"
+          element={<ForgottenPasswordPage />}
+        />
+        <Route path="/prodavnica" element={<ProductDetailsPage />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
-
